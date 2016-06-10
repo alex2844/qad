cf96ef36aff3cfbbcb850a00722063ac73ad418f
Alex Smith <alex.smith2844@gmail.com>
2016-06-10 10:23:12 +0500

    fix style cdn

--- data/qad/qad.js
+++ data/qad/qad.js
@@ -831,7 +831,8 @@ var Qad={
 					style = xhr.responseText;
 					if (location.origin == 'file://')
 						style = style.replace(/\@location/g,location.href.split('/page')[0]);
-					else if (Qad.$('link[rel="stylesheet/qad"]').href.indexOf('cdn') != -1)
+					//else if (Qad.$('link[rel="stylesheet/qad"]').href.indexOf('cdn') != -1)
+					else if (Qad.$('link[rel="stylesheet/qad"]').href.indexOf('rawgit') != -1)
 						style = style.replace(/\@location/g,Qad.$('link[rel="stylesheet/qad"]').href.split('data')[0]);
 					else
 						style = style.replace(/\@location/g,location.origin);
