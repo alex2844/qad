19f105244272d0994bf4792db92524ed49516750
Alex Smith <alex.smith2844@gmail.com>
2016-06-27 09:11:52 +0000

    google otp

--- data/qad/qad.php
+++ data/qad/qad.php
@@ -28,6 +28,40 @@ class Qad{
 			return false;
 		return true;
 	}
+	private function getCode($secret, $timeSlice, $base32chars) {
+		if (empty($secret))
+			return '';
+		$base32charsFlipped = array_flip($base32chars);
+		$paddingCharCount = substr_count($secret, '=');
+		$allowedValues = array(6, 4, 3, 1, 0);
+		if (!in_array($paddingCharCount, $allowedValues))
+			return false;
+		for ($i = 0; $i < 4; $i++)
+			if ($paddingCharCount == $allowedValues[$i] && substr($secret, -($allowedValues[$i])) != str_repeat('=', $allowedValues[$i]))
+				return false;
+		$secret = str_replace('=','', $secret);
+		$secret = str_split($secret);
+		$secretkey = '';
+		for ($i = 0; $i < count($secret); $i = $i+8) {
+			$x = '';
+			if (!in_array($secret[$i], $base32chars))
+				return false;
+			for ($j = 0; $j < 8; $j++)
+				$x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);
+			$eightBits = str_split($x, 8);
+			for ($z = 0; $z < count($eightBits); $z++)
+				$secretkey .= ( ($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48 ) ? $y:"";
+		}
+		$time = chr(0).chr(0).chr(0).chr(0).pack('N*', $timeSlice);
+		$hm = hash_hmac('SHA1', $time, $secretkey, true);
+		$offset = ord(substr($hm, -1)) & 0x0F;
+		$hashpart = substr($hm, $offset, 4);
+		$value = unpack('N', $hashpart);
+		$value = $value[1];
+		$value = $value & 0x7FFFFFFF;
+		$modulo = pow(10, 6);
+		return str_pad($value % $modulo, 6, '0', STR_PAD_LEFT);
+	}
 
 	public function passport($data,$key='',$scope='') {
 		if (!is_array($data))
@@ -48,6 +82,36 @@ class Qad{
 		$passport = json_encode($passport);
 		setcookie('passport'.($key!=''?'.'.$key:''), $passport, time()+60*60*24*30, '/');
 		return $passport;
+	}
+	public function otp($p1,$p2) {
+		$validChars =  array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '=');
+		if ($p1 == 'secret') {
+			unset($validChars[32]);
+			$secret = '';
+			for ($i = 0; $i < 16; $i++)
+				$secret .= $validChars[array_rand($validChars)];
+			if (!empty(self::$config['name']))
+				$name = self::$config['name'];
+			else
+				$name = $_SERVER['HTTP_HOST'];
+    	    return [
+				'secret' => $secret,
+				'qr' => 'otpauth://totp/'.$p2.'?secret='.$secret.'&issuer='.$name
+			];
+		}else{
+			if (empty($p2))
+				return ['check'=>false];
+			$currentTimeSlice = floor(time() / 30);
+			$calculatedCode = $this->getCode($p1, $currentTimeSlice,$validChars);
+			if ($calculatedCode == $p2)
+				return ['check'=>true];
+			else
+				return ['chack'=>false];
+		}
+	}
+	public function qr($method,$code) {
+		if ($method == 'get')
+			return 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl='.urlencode($code);
 	}
 	public function shab() {
 		$file = file_get_contents('index.html');
