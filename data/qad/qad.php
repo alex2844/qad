9f00d6afebfcf5c8cb0d596272307f2405195526
Alex Smith <alex.smith2844@gmail.com>
2016-11-11 23:15:21 +0500

    qad.php -> Qad::furl -> friendly URL

--- data/qad/qad.php
+++ data/qad/qad.php
@@ -103,6 +103,18 @@ class Qad{
 		$_SESSION['passport'.($key!=''?'::'.$key:'')] = md5($passport);
 		return $passport;
 	}
+	public function furl($s) {
+		$s = (string) $s;
+		$s = strip_tags($s);
+		$s = str_replace(array("\n", "\r"), " ", $s);
+		$s = preg_replace("/\s+/", ' ', $s);
+		$s = trim($s);
+		$s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s);
+		$s = strtr($s, array('а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'j','з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c','ч'=>'ch','ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya','ъ'=>'','ь'=>''));
+		$s = preg_replace("/[^0-9a-z-_ ]/i", "", $s);
+		$s = str_replace(" ", "-", $s);
+		return $s;
+	}
 	public function otp($p1,$p2) {
 		$validChars =  array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '=');
 		if ($p1 == 'secret') {
