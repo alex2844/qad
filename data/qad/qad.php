4d76bca1269bc486d315d5c126989578dd2ed672
Alex Smith <alex.smith2844@gmail.com>
2016-11-13 00:31:03 +0500

    Qad::keywords((string|array)$srt,(array)$skip)

--- data/qad/qad.php
+++ data/qad/qad.php
@@ -117,6 +117,32 @@ class Qad{
 		$s = str_replace(" ", "-", $s);
 		return $s;
 	}
+	public function keywords($str,$skip='') {
+		if (gettype($str) == 'array')
+			$str = implode(' ',$str);
+		$str = mb_strtolower($str);
+		if (!empty($skip))
+			$skip = array_map('mb_strtolower', $skip);
+		$str = @preg_replace(array("'<[\/\!]*?[^<>]*?>'si","'([\r\n])[\s]+'si","'&[a-z0-9]{1,6};'si","'( +)'si"),array("","\\1 "," "," "),strip_tags($str));
+		$rearray = array("~","!","@","#","$","%","^","&","*","(",")","_","+","`",'"',"№",";",":","?","-","=","|","\"","\\","/","[","]","{","}","'",",",".","<",">","\r\n","\n","\t","«","»");
+		$str = @str_replace($rearray," ",$str);
+		$keywordcache = @explode(" ",$str);
+		$rearray = array();
+		foreach ($keywordcache as $word) {
+			if (mb_strlen($word) >= 5 && !is_numeric($word)) {
+				$adjective = mb_substr($word,-2);
+				if (empty($skip) || (!in_array($adjective,$skip) && !in_array($word,$skip)))
+					$rearray[$word] = (array_key_exists($word,$rearray)) ? ($rearray[$word] + 1) : 1;
+			}
+		}
+		@arsort($rearray);
+		$keywordcache = @array_slice($rearray,0,10);
+		$keywords = "";
+		foreach ($keywordcache as $word=>$count) {
+			$keywords.= ",".$word;
+		}
+		return substr($keywords,1);
+	}
 	public function otp($p1,$p2) {
 		$validChars =  array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '=');
 		if ($p1 == 'secret') {
