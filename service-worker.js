ead927adefc1196c1d776de6b7ce16de7942a25f
Alex Smith <alex.smith2844@gmail.com>
2017-09-29 01:41:54 +0500

    auto update cache

--- service-worker.js
+++ service-worker.js
@@ -12,56 +12,55 @@ var BLACKLIST = [
 	],
 	FILES = [],
 	CACHENAME = location.search.split('?')[1],
-	UPDATE = upday();
-self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open(UPDATE).then(cache => {
-			console.log('Install app');
-			return cache.addAll(FILES);
-		})
-	);
+	UPDATE = _upday();
+self.addEventListener('install', e => {
+	e.waitUntil(caches.open(UPDATE).then(cache => {
+		console.log('Install app');
+		return cache.addAll(FILES);
+	}));
 });
-self.addEventListener('activate', event => {
-	event.waitUntil(
-		caches.keys().then(keys => {
-			return Promise.all(keys.map((key, i) => {
-				if(key !== UPDATE)
-					return caches.delete(keys[i]);
-			}))
-		})
-	);
+self.addEventListener('activate', e => {
+	e.waitUntil(_clear);
 });
-self.addEventListener('fetch', event => {
-	event.respondWith(
-		caches.match(event.request).then(response => {
-			return response || fetch(event.request).then(response => caches.open(UPDATE).then(cache => {
-				if (response.status == 200 && event.request.method == 'GET' && !(event.request.mode == 'cors' && !event.request.url.match('/data/')))
-					cache.put(event.request, response.clone());
-				return response;
-			}));
-		})
-	);
+self.addEventListener('fetch', e => {
+	e.respondWith(caches.match(e.request).then(res => {
+		if (res && (Date.now() - new Date(res.headers.get('date')).valueOf() > 86400000) && navigator.onLine)
+			_clear(true);
+		return res || fetch(e.request).then(res => caches.open(UPDATE).then(cache => {
+			if (res.status == 200 && e.request.method == 'GET' && !(e.request.mode == 'cors' && !e.request.url.match('/data/')))
+				cache.put(e.request, res.clone());
+			return res;
+		}));
+	}));
 });
-self.addEventListener('notificationclick', event => {
-	console.log('Notification click: tag ', event.notification.tag);
-	event.notification.close();
-	if (!event.action)
+self.addEventListener('notificationclick', e => {
+	console.log('Notification click: tag ', e.notification.tag);
+	e.notification.close();
+	if (!e.action)
 		return;
-	event.waitUntil(
+	e.waitUntil(
 		clients.matchAll({
 			type: 'window'
 		}).then(windowClients => {
 			for (var i = 0; i < windowClients.length; i++) {
 				var client = windowClients[i];
-				if (client.url === location.origin+CACHENAME+'?actions='+event.action && 'focus' in client)
+				if (client.url === location.origin+CACHENAME+'?actions='+e.action && 'focus' in client)
 					return client.focus();
 			}
 			if (clients.openWindow)
-				return clients.openWindow(location.origin+CACHENAME+'?actions='+event.action);
+				return clients.openWindow(location.origin+CACHENAME+'?actions='+e.action);
 		})
 	);
 });
-function upday() {
+function _clear(e) {
+	return caches.keys().then(keys => {
+		return Promise.all(keys.map((key, i) => {
+			if ((Number(key) !== UPDATE) || e == true)
+				return caches.delete(keys[i]);
+		}))
+	});
+}
+function _upday() {
 	var now = new Date(),
 		start = new Date(now.getFullYear(), 0, 0),
 		diff = now - start,
