c59cd880071a28a4f9d57fcf5fb66765e67b0925
Alex Smith <alex.smith2844@gmail.com>
2017-10-13 12:50:20 +0500

    del: md5 && devtools, done: translate && css

--- data/qad/qad.js
+++ data/qad/qad.js
@@ -255,7 +255,20 @@ var Qad={
 			return obj.parentNode.removeChild(obj);
 		}
 		obj.css = function(style) {
-			Object.assign(obj.style, style);
+			if (obj.tagName == 'STYLE') {
+				var css = '';
+				for (var i in style) {
+					css += i+Qad.replace([
+						/"/g,
+						/,/g
+					], [
+						'',
+						';'
+					], Qad.json(style[i]));
+				}
+				obj.$(css);
+			}else
+				Object.assign(obj.style, style);
 			return obj;
 		}
 		obj.template = function(d,t) {
@@ -1471,177 +1484,6 @@ var Qad={
 			Qad.$('link[rel="stylesheet/qad"]').rel = 'stylesheet';
 		},style);
 	},
-	md5: function(s) {
-		var binl_md5 = function(x, len) {
-			var bit_rol = function(num, cnt) {
-				return (num << cnt) | (num >>> (32 - cnt));
-			}
-			var md5_cmn = function(q, a, b, x, s, t) {
-				return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s),b);
-			}
-			var md5_ff = function(a, b, c, d, x, s, t) {
-				return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t);
-			}
-			var md5_gg = function(a, b, c, d, x, s, t) {
-				return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t);
-			}
-			var md5_hh = function(a, b, c, d, x, s, t) {
-				return md5_cmn(b ^ c ^ d, a, b, x, s, t);
-			}
-			var md5_ii = function(a, b, c, d, x, s, t) {
-				return md5_cmn(c ^ (b | (~d)), a, b, x, s, t);
-			}
-			var safe_add = function(x, y) {
-				var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-				var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
-				return (msw << 16) | (lsw & 0xFFFF);
-			}
-			x[len >> 5] |= 0x80 << ((len) % 32);
-			x[(((len + 64) >>> 9) << 4) + 14] = len;
-			var a =  1732584193;
-			var b = -271733879;
-			var c = -1732584194;
-			var d =  271733878;
-			for(var i = 0; i < x.length; i += 16) {
-				var olda = a;
-				var oldb = b;
-				var oldc = c;
-				var oldd = d;
-				a = md5_ff(a, b, c, d, x[i+ 0], 7 , -680876936);
-				d = md5_ff(d, a, b, c, x[i+ 1], 12, -389564586);
-				c = md5_ff(c, d, a, b, x[i+ 2], 17,  606105819);
-				b = md5_ff(b, c, d, a, x[i+ 3], 22, -1044525330);
-				a = md5_ff(a, b, c, d, x[i+ 4], 7 , -176418897);
-				d = md5_ff(d, a, b, c, x[i+ 5], 12,  1200080426);
-				c = md5_ff(c, d, a, b, x[i+ 6], 17, -1473231341);
-				b = md5_ff(b, c, d, a, x[i+ 7], 22, -45705983);
-				a = md5_ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
-				d = md5_ff(d, a, b, c, x[i+ 9], 12, -1958414417);
-				c = md5_ff(c, d, a, b, x[i+10], 17, -42063);
-				b = md5_ff(b, c, d, a, x[i+11], 22, -1990404162);
-				a = md5_ff(a, b, c, d, x[i+12], 7 ,  1804603682);
-				d = md5_ff(d, a, b, c, x[i+13], 12, -40341101);
-				c = md5_ff(c, d, a, b, x[i+14], 17, -1502002290);
-				b = md5_ff(b, c, d, a, x[i+15], 22,  1236535329);
-				a = md5_gg(a, b, c, d, x[i+ 1], 5 , -165796510);
-				d = md5_gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
-				c = md5_gg(c, d, a, b, x[i+11], 14,  643717713);
-				b = md5_gg(b, c, d, a, x[i+ 0], 20, -373897302);
-				a = md5_gg(a, b, c, d, x[i+ 5], 5 , -701558691);
-				d = md5_gg(d, a, b, c, x[i+10], 9 ,  38016083);
-				c = md5_gg(c, d, a, b, x[i+15], 14, -660478335);
-				b = md5_gg(b, c, d, a, x[i+ 4], 20, -405537848);
-				a = md5_gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
-				d = md5_gg(d, a, b, c, x[i+14], 9 , -1019803690);
-				c = md5_gg(c, d, a, b, x[i+ 3], 14, -187363961);
-				b = md5_gg(b, c, d, a, x[i+ 8], 20,  1163531501);
-				a = md5_gg(a, b, c, d, x[i+13], 5 , -1444681467);
-				d = md5_gg(d, a, b, c, x[i+ 2], 9 , -51403784);
-				c = md5_gg(c, d, a, b, x[i+ 7], 14,  1735328473);
-				b = md5_gg(b, c, d, a, x[i+12], 20, -1926607734);
-				a = md5_hh(a, b, c, d, x[i+ 5], 4 , -378558);
-				d = md5_hh(d, a, b, c, x[i+ 8], 11, -2022574463);
-				c = md5_hh(c, d, a, b, x[i+11], 16,  1839030562);
-				b = md5_hh(b, c, d, a, x[i+14], 23, -35309556);
-				a = md5_hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
-				d = md5_hh(d, a, b, c, x[i+ 4], 11,  1272893353);
-				c = md5_hh(c, d, a, b, x[i+ 7], 16, -155497632);
-				b = md5_hh(b, c, d, a, x[i+10], 23, -1094730640);
-				a = md5_hh(a, b, c, d, x[i+13], 4 ,  681279174);
-				d = md5_hh(d, a, b, c, x[i+ 0], 11, -358537222);
-				c = md5_hh(c, d, a, b, x[i+ 3], 16, -722521979);
-				b = md5_hh(b, c, d, a, x[i+ 6], 23,  76029189);
-				a = md5_hh(a, b, c, d, x[i+ 9], 4 , -640364487);
-				d = md5_hh(d, a, b, c, x[i+12], 11, -421815835);
-				c = md5_hh(c, d, a, b, x[i+15], 16,  530742520);
-				b = md5_hh(b, c, d, a, x[i+ 2], 23, -995338651);
-				a = md5_ii(a, b, c, d, x[i+ 0], 6 , -198630844);
-				d = md5_ii(d, a, b, c, x[i+ 7], 10,  1126891415);
-				c = md5_ii(c, d, a, b, x[i+14], 15, -1416354905);
-				b = md5_ii(b, c, d, a, x[i+ 5], 21, -57434055);
-				a = md5_ii(a, b, c, d, x[i+12], 6 ,  1700485571);
-				d = md5_ii(d, a, b, c, x[i+ 3], 10, -1894986606);
-				c = md5_ii(c, d, a, b, x[i+10], 15, -1051523);
-				b = md5_ii(b, c, d, a, x[i+ 1], 21, -2054922799);
-				a = md5_ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
-				d = md5_ii(d, a, b, c, x[i+15], 10, -30611744);
-				c = md5_ii(c, d, a, b, x[i+ 6], 15, -1560198380);
-				b = md5_ii(b, c, d, a, x[i+13], 21,  1309151649);
-				a = md5_ii(a, b, c, d, x[i+ 4], 6 , -145523070);
-				d = md5_ii(d, a, b, c, x[i+11], 10, -1120210379);
-				c = md5_ii(c, d, a, b, x[i+ 2], 15,  718787259);
-				b = md5_ii(b, c, d, a, x[i+ 9], 21, -343485551);
-				a = safe_add(a, olda);
-				b = safe_add(b, oldb);
-				c = safe_add(c, oldc);
-				d = safe_add(d, oldd);
-			}
-			return Array(a, b, c, d);
-		}
-		var str2rstr_utf8 = function(input) {
-			var output = "";
-			var i = -1;
-			var x, y;
-			while(++i < input.length) {
-				x = input.charCodeAt(i);
-				y = i + 1 < input.length ? input.charCodeAt(i + 1) : 0;
-				if(0xD800 <= x && x <= 0xDBFF && 0xDC00 <= y && y <= 0xDFFF) {
-					x = 0x10000 + ((x & 0x03FF) << 10) + (y & 0x03FF);
-					i++;
-				}
-				if(x <= 0x7F)
-					output += String.fromCharCode(x);
-				else if(x <= 0x7FF)
-					output += String.fromCharCode(0xC0 | ((x >>> 6 ) & 0x1F), 0x80 | ( x & 0x3F));
-				else if(x <= 0xFFFF)
-				output += String.fromCharCode(0xE0 | ((x >>> 12) & 0x0F), 0x80 | ((x >>> 6 ) & 0x3F), 0x80 | ( x & 0x3F));
-				else if(x <= 0x1FFFFF)
-					output += String.fromCharCode(0xF0 | ((x >>> 18) & 0x07), 0x80 | ((x >>> 12) & 0x3F), 0x80 | ((x >>> 6 ) & 0x3F), 0x80 | (x & 0x3F));
-			}
-			return output;
-		}
-		var rstr2binl = function(input) {
-			var output = Array(input.length >> 2);
-			for(var i = 0; i < output.length; i++)
-				output[i] = 0;
-			for(var i = 0; i < input.length * 8; i += 8)
-				output[i>>5] |= (input.charCodeAt(i / 8) & 0xFF) << (i%32);
-			return output;
-		}
-		s = str2rstr_utf8(s);
-		s = binl_md5(rstr2binl(s), s.length * 8);
-		var output = "";
-		for(var i = 0; i < s.length * 32; i += 8)
-			output += String.fromCharCode((s[i>>5] >>> (i % 32)) & 0xFF);
-		s = output;
-		hexcase = 0;
-		var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
-		var output = "";
-		var x;
-		for(var i = 0; i < s.length; i++) {
-			x = s.charCodeAt(i);
-			output += hex_tab.charAt((x >>> 4) & 0x0F) + hex_tab.charAt( x & 0x0F);
-		}
-		return output;
-	},
-	devtool: function(c) {
-		var devtools = false;
-		setInterval(function () {
-			if ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) ||
-				window.outerWidth - window.innerWidth > 160) {
-				if (!devtools)
-					c(true);
-				devtools = true;
-			}else{
-				if (devtools)
-					c(false);
-				devtools = false;
-			}
-		}, 500);
-		if (!window.devtools)
-			c(devtools);
-		window.devtools = devtools;
-	},
 	init: function(type, action) {
 		switch (type) {
 			case 'button#menu + nav': {
@@ -2108,24 +1950,59 @@ window.addEventListener('load',function() {
 		}
 	}
 	if (Qad.$('html').lang) {
-		var script = Qad.$('/script');
-		document.translate = function() {
+		document.translate = () => {
 			if (Qad.$('#google_translate') && Qad.$('#google_translate').$() == '')
 				Qad.$('#google_translate').$('&#xE8E2').attr('class','material-icons');
-			Qad.for('.material-icons',function(el) {
+			Qad.for('.material-icons', el => {
 				el.classList.add('notranslate');
 			});
 			if (Qad.$('footer'))
 				Qad.$('footer').classList.add('notranslate');
+			if (Qad.$('html').lang != navigator.language && !Qad.session.get('googtrans'))
+				Qad.session.set('googtrans', '/'+Qad.$('html').lang+'/'+navigator.language);
 			new google.translate.TranslateElement({
 				pageLanguage: Qad.$('html').lang,
 				layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
 				gaTrack: true,
 				gaId: Qad.$('meta[name="analytics"]').content
 			}, 'google_translate');
+			setTimeout(() => {
+				if (!Qad.$('iframe.goog-te-menu-frame'))
+					return;
+				var color = Qad.$('meta[name="theme-color"]').content;
+				Qad.$('iframe.goog-te-menu-frame').css({
+					position: 'absolute',
+					'max-width': '100%',
+					'margin-top': '10px',
+					'border-radius': '0.125rem',
+					'box-shadow': '0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)'
+				});
+				Qad.$('iframe.goog-te-menu-frame body').add($('/style').css({
+					'::-webkit-scrollbar': {
+						width: '5px',
+						height: '5px'
+					},
+					'::-webkit-scrollbar-thumb': {
+						background: color,
+						'border-radius': '10px'
+					},
+					'.goog-te-menu2': {
+						height: 'auto !important',
+						border: '1px solid #ccc',
+						'max-width': '100%',
+						'overflow-x': 'auto'
+					},
+					'.goog-te-menu2-item div, .goog-te-menu2-item:link div': {
+						color: color
+					},
+					'.goog-te-menu2-item:hover div': {
+						background: color,
+						color: 'fff'
+					}
+				}));
+			}, 1000);
 		}
-		script.src = '//translate.google.com/translate_a/element.js?cb=document.translate';
-		Qad.$('head').add(script);
+		Qad.$('head').add($('/script').attr('src', '//translate.google.com/translate_a/element.js?cb=document.translate'));
 	}
 	if (navigator.serviceWorker && !Qad.$('html[dev]')) {
 		navigator.serviceWorker.register('/service-worker.js?'+location.pathname, {scope: location.pathname}).then(_sw => {
